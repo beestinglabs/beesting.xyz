1f6db36e0a8e5f519f4907e592604c10a1cc7568
Ed Powley <edpowley@gmail.com>
2019-05-15 14:32:41 +0100

    Updates

---
+++ precache-manifest.3755cdc7e18ebfe1d6a7caa75fdfee3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db70ce57d5435a33de3e44ccf21d493",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3776380b1de968366039",
+    "url": "/static/css/main.2c5772e2.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "3776380b1de968366039",
+    "url": "/static/js/main.cab96705.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.3755cdc7e18ebfe1d6a7caa75fdfee3e.js"
 );
 
 self.addEventListener('message', (event) => {
