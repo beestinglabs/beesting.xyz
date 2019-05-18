7f939eba5049f4a8b8294f9993b926ddaa52a42f
Ed Powley <edpowley@gmail.com>
2019-05-18 12:04:10 +0100

    Embed hiveSynth video

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <p>beesting labs</p>
         <p><a class="App-link" href="https://norahlorway.com">dr norah lorway</a> + <a class="App-link" href="https://edpowley.com">dr ed powley</a></p>
-        <p>coming soon</p>
+        <p>
+            <iframe src="https://player.vimeo.com/video/336929104" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+        </p>
       </header>
     </div>
   );
