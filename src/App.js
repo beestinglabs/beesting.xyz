8d6b60ebff5751e3042ec0fb9579ac35dba7aec5
Ed Powley <edpowley@gmail.com>
2022-06-08 16:56:38 +0100

    Added secton on Scorch

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>beesting labs</p>
+        <h1>beesting labs</h1>
         <p class="small-text">makes software and hardware for audio etc.</p>
         <p class="small-text">it is run and founded by:</p>
         <p><a class="App-link" href="https://norahlorway.com">dr norah lorway</a> + <a class="App-link" href="https://edpowley.com">dr ed powley</a> + <a class="App-link" href="https://soundcloud.com/thonhallion">arthur wilson</a></p>
-        <p class="small-text">here's what we're working on right now: hiveSynth<br />a virtual modular analogue synthesiser with a unique virtual/augmented/mixed reality interface</p>
+        <p class="small-text">here's what we're working on right now:</p>
+        <h2>scorch</h2>
+        <p class="small-text">a domain-specific programming language for music and sound, integrating with your existing DAW and synth plugins</p>
+        <p class="small-text"><a href="https://www.scorch.live/">website</a></p>
+        <h2>hiveSynth</h2>
+        <p class="small-text">a virtual modular analogue synthesiser with a unique virtual/augmented/mixed reality interface</p>
         <div class="col-12 video-container">
           <div class="embed-responsive embed-responsive-16by9 text-center">
             <iframe class="embed-responsive-item video" src="https://player.vimeo.com/video/336929104" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
